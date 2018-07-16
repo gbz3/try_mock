42a90bdcc33f4d6d946ca97101fe7cb4a859886d
gbz3 <gebizo3@gmail.com>
2018-07-16 10:38:18 +0900

    init

---
+++ src/main/java/try_mock/MyCmd.java
@@ -0,0 +1,10 @@
+package try_mock;
+
+public class MyCmd {
+
+	public static void main(String[] args) {
+		System.out.println( "Hello World!" );
+		System.exit( 0 );
+	}
+
+}
---
+++ src/test/java/try_mock/MyCmdTest.java
@@ -0,0 +1,15 @@
+package try_mock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyCmdTest {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+		assertTrue( true );
+	}
+
+}
